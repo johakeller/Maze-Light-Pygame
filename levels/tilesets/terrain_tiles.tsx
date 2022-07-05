7948f202eb63bdea7d2f92f10e231e0b5e5a4e8d
Johannes Keller <86203285+johakeller@users.noreply.github.com>
2022-07-05 17:26:26 +0200

    05.07.2022 levels

---
+++ levels/tilesets/terrain_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="wall-tiles" tilewidth="64" tileheight="64" tilecount="25" columns="5">
+ <image source="../../graphics/terrain/wall_tiles.png" width="320" height="320"/>
+</tileset>
